1b491d7a8220ee55c27ee6f79897b3f4df785981
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2026-04-08 00:30:13 -0500

    feat. Header con descripciones para buscador

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
 import HeroSection from '../components/HeroSection';
 import PresentationSection from '../components/PresentationSection';
 import InvitationSection from '../components/InvitationSection';
@@ -8,6 +9,13 @@ import ContactSection from '../components/ContactSection';
 const HomePage: React.FC = () => {
   return (
     <>
+      <Helmet>
+        <title>Iniciativa SPARK - Proyectos Tecnologicos para Aprender</title>
+        <meta
+          name="description"
+          content="Aprende en la práctica desarrollando proyectos tecnológicos creativos e innovadores. Somos un Club de Universidad San Ignacio de Loyola - USIL orientada a ampliar las habilidades técnicas y poner en práctica las habilidades blandas dentro de proyectos de software. Inscríbete en los proyectos de la Iniciativa SPARK Usil y nosotros encontraremos integrantes con tus mismas aficiones."
+        />
+      </Helmet>
       <div id="hero">
         <HeroSection />
       </div>
